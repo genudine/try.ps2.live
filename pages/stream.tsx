ca6fb7b0b79f43a0a0661777ead480f28bfa5337
Katalina Okano <git@kat.cafe>
2022-12-22 00:18:26 -0500

    add pre-stream meta messages for example

--- pages/stream.tsx
+++ pages/stream.tsx
@@ -1,9 +1,13 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import ESSConfigurator, {
   Config,
 } from "../components/ESSConfigurator/ESSConfigurator";
 import styles from "../styles/Stream.module.css";
 import { useRouter } from "next/router";
+import { Fira_Code } from "@next/font/google";
+import { c } from "../utils/css";
+
+const firaCode = Fira_Code({ subsets: ["latin"] });
 
 export default function Stream() {
   const router = useRouter();
@@ -17,7 +21,11 @@ export default function Stream() {
     events: [],
     expFilterRaw: "",
     logicalAndCharactersWithWorlds: false,
+    // ...JSON.parse((router.query.c as string) || "{}"), // If server can see it, lets try it...
   });
+  const [streamState, setStreamState] = useState<"stopped" | "started">(
+    "stopped"
+  );
 
   useEffect(() => {
     if (router.query.c) {
@@ -32,6 +40,7 @@ export default function Stream() {
       }
     }
     // If we add config as a dep, we'll get an infinite loop
+    // TODO: make less bad
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [router.query.c]);
 
@@ -48,15 +57,69 @@ export default function Stream() {
     <div className={styles.main}>
       <div className={styles.grid}>
         <div>
-          <div className={styles.panelHead}>STREAM CONFIG</div>
+          <div className={c(styles.panelHead)}>-&gt; CONFIGURATOR</div>
           <ESSConfigurator
             config={config}
             onUpdate={configUpdate}
-            onStartStop={() => {}}
+            onStartStop={() => {
+              setStreamState(streamState === "started" ? "stopped" : "started");
+            }}
           />
         </div>
         <div>
-          <div className={styles.panelHead}>STREAM DATA</div>
+          <div className={c(styles.panelHead)}>
+            -&gt; STREAM DATA [{" "}
+            <span
+              style={{
+                color: streamState === "started" ? "limegreen" : "grey",
+              }}
+            >
+              {streamState === "started" ? "RUNNING" : "STOPPED"}
+            </span>{" "}
+            ]
+          </div>
+          <p className={c(firaCode.className, styles.stream)}>
+            <pre>
+              &lt;&lt;==[META] Will connect to {config.baseUrl}?environment=
+              {config.environment}&service-id=s:{config.serviceID}
+              <br />
+              &lt;&lt;==[META] Will send message after connect:
+              <br />
+              {JSON.stringify(
+                {
+                  service: "event",
+                  action: "subscribe",
+                  characters: useMemo(() => {
+                    if (config.charactersRaw) {
+                      return config.charactersRaw
+                        .split(",")
+                        .map((x) => x.trim());
+                    }
+
+                    return ["all"];
+                  }, [config.charactersRaw]),
+                  eventNames: useMemo(() => {
+                    let events = [...config.events];
+                    if (
+                      config.events.includes("GainExperience") &&
+                      config.expFilterRaw
+                    ) {
+                      events = events.filter((x) => x !== "GainExperience");
+                      config.expFilterRaw.split(",").forEach((x) => {
+                        events.push(`GainExperience_experience_id_${x.trim()}`);
+                      });
+                    }
+                    return events;
+                  }, [config.events, config.expFilterRaw]),
+                  worlds: config.worlds === "all" ? ["all"] : config.worlds,
+                  logicalAndCharactersWithWorlds:
+                    config.logicalAndCharactersWithWorlds,
+                },
+                null,
+                2
+              )}
+            </pre>
+          </p>
         </div>
       </div>
     </div>
